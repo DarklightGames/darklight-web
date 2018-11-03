8d13573aa43c595d24ad43d6a678516f21ebcbc6
Colin Basnett <cmbasnett@gmail.com>
2018-11-03 11:57:50 -0700

    Added a heatmap for kills.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Map, ImageOverlay } from 'react-leaflet'
+import HeatmapLayer from 'react-leaflet-heatmap-layer'
+
 import ReactTable from 'react-table'
 import logo from './logo.svg';
+import L from 'leaflet'
 
 import './App.css';
 import "react-table/react-table.css";
@@ -10,7 +14,7 @@ class Api {
     this.host = host
   }
 
-  get(path, params=null) {
+  get(path, params = null) {
     var url = new URL(this.host + '/' + path)
     if (params) {
       Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
@@ -33,6 +37,7 @@ class App extends Component {
       data: [],
       pages: 0,
       loading: false,
+      frags: []
     }
   }
 
@@ -46,16 +51,15 @@ class App extends Component {
         limit: pageSize,
         offset: page * pageSize
       }).then(response => response.json())
-      .then(data => {
-        const res = {
-          rows: data.results,
-          pages: Math.ceil(data.count / pageSize)
-        }
-        resolve(res)
-      })
+        .then(data => {
+          const res = {
+            rows: data.results,
+            pages: Math.ceil(data.count / pageSize)
+          }
+          resolve(res)
+        })
     })
   }
-  
 
   fetchData(state, instance) {
     this.requestData(
@@ -72,9 +76,42 @@ class App extends Component {
     })
   }
 
+  fetchFrags() {
+    let scope = this
+    api.get('frags/', {
+      map_id: 'DH-Stoumont',
+      limit: 10000
+    }).then(response => response.json())
+    .then(data => {
+      scope.setState({frags: data.results})
+    })
+  }
+
+  componentDidMount() {
+    this.fetchFrags()
+  }
+
   render() {
     return (
       <div className="App">
+        <Map
+          style={{height: 1024}}
+          bounds={[[32768,-16384], [-16384,32768]]}
+          crs={L.CRS.Simple}
+          minZoom={-7}
+        >
+          <ImageOverlay
+            url='stoumont.png'
+            bounds={[[32768,-16384], [-16384,32768]]}
+          />
+          <HeatmapLayer
+            minOpacity={0.25}
+            points={this.state.frags}
+            longitudeExtractor={m => m['killer_location'][0]}
+            latitudeExtractor={m => 16384 - m['killer_location'][1]}
+            intensityExtractor={m => 2}
+          />
+        </Map>
         <ReactTable
           defaultPageSize={20}
           columns={[
